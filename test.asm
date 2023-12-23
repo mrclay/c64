c579018d57460af143de6a3ec08bba3590912d80
Steve Clay <steve@mrclay.org>
2023-12-23 01:01:56 -0500

    Simplification

--- test.asm
+++ test.asm
@@ -16,22 +16,11 @@
   CHAR_ROM          = $D000
   MOVE_CHAR_BY      = 40
 
-  FIRST_SPRITE_IDX = 1
-  LAST_SPRITE_IDX = 6
-
   SPRITE_DATA_L = $80
   SPRITE_DATA_R = $81
   SPRITE_DATA_SQUARE = $82
 
   SPRITE_PTR1 = $07F8
-  SPRITE_PTR2 = $07F9
-  SPRITE_PTR3 = $07FA
-  SPRITE_PTR4 = $07FB
-  SPRITE_PTR5 = $07FC
-  SPRITE_PTR6 = $07FD
-  SPRITE_PTR7 = $07FE
-  SPRITE_PTR8 = $07FF
-
   SPRITE_X1 = $D000
   SPRITE_X_HIGH = $D010
 
@@ -39,7 +28,7 @@
 
   ; How many lines before the target raster line before we start
   ; moving sprites
-  PREP_LINES = 12
+  PREP_LINES = 15
 
 main
   jsr SR_screen_setup
@@ -59,31 +48,31 @@ SR_screen_setup
 
   ; scale all 8 sprites x2
   lda #%11111111
-	sta $D01D	; x-axis
-	sta $D017	; y-axis
+  sta $D01D ; x-axis
+  sta $D017 ; y-axis
 
   ; sprite FG colors
-	!for i, 0, 7 {
-    lda #0
+  !for i, 0, 7 {
+    lda #i + 1
     sta $D027 + i
   }
 
   ; point to our sprite data
   lda #SPRITE_DATA_SQUARE
   sta SPRITE_PTR1
-  sta SPRITE_PTR2
-  sta SPRITE_PTR3
-  sta SPRITE_PTR6
-  sta SPRITE_PTR7
-  sta SPRITE_PTR8
+  sta SPRITE_PTR1 + 1
+  sta SPRITE_PTR1 + 2
+  sta SPRITE_PTR1 + 5
+  sta SPRITE_PTR1 + 6
+  sta SPRITE_PTR1 + 7
   lda #SPRITE_DATA_L
-  sta SPRITE_PTR4
+  sta SPRITE_PTR1 + 3
   lda #SPRITE_DATA_R
-  sta SPRITE_PTR5
+  sta SPRITE_PTR1 + 4
 
   ; turn on sprites
-  lda #%01111110
-	sta $d015
+  lda #%10111101
+  sta $d015
 
   ; sprite X values
   ldy #0
@@ -95,9 +84,10 @@ SR_screen_setup
   sta SPRITE_X_HIGH
 
   ; sprite Y values
-  lda #50
+  ldy #0
+  lda sprite_y_values, y
   !for i, 0, 7 {
-    sta SPRITE_X1 + (i * 2) + 1
+    sta SPRITE_X1 + 1 + (2 * i)
   }
 
   jsr SR_fill_screen
@@ -127,21 +117,21 @@ irq_handler
   jmp .done
 
 .irq_not_5
-  ; sprite Y values
-  ldy active_interrupt
-  lda sprite_y_values, y
-  !for i, FIRST_SPRITE_IDX, LAST_SPRITE_IDX {
-    sta SPRITE_X1 + 1 + (2 * i)
-  }
-
   ; sprite X values
   ldy active_interrupt
-  !for i, FIRST_SPRITE_IDX, LAST_SPRITE_IDX {
+  !for i, 0, 7 {
     lda sprite_x1_low_values + (5 * i), y
     sta SPRITE_X1 + (i * 2)
   }
   lda sprite_x_high_values, y
   sta SPRITE_X_HIGH
+
+  ; sprite Y values
+  ldy active_interrupt
+  !for i, 0, 7 {
+    lda sprite_y_values + (5 * i), y
+    sta SPRITE_X1 + 1 + (i * 2)
+  }
 
 .done
   ; set up for next rasterline
@@ -166,57 +156,12 @@ SR_after_screen
   rts
 
 
-!zone
-; Sets A to 1 if end of screen, otherwise 0
-bump_pointers
-  inc PTR1
-  inc PTR2
-
-  jmp screen_end_check
-after_screen_end_check
-  cmp #1
-  beq .return_1
-
-  ; check for low byte wrapping
-  lda PTR1
-  ; If not 0, we did not wrap
-  cmp #0
-  bne .return_0
-
-  ; rollover high byte
-  lda #0
-  sta PTR1
-  sta PTR2
-  inc PTR1_HIGH
-  inc PTR2_HIGH
-.return_0
-  lda #0
-  jmp after_bump_pointers
-.return_1
-  lda #1
-  jmp after_bump_pointers
-
-
-!zone
-screen_end_check
-  lda PTR1 + 1
-  cmp #7
-  bne .return_0
-  lda PTR1
-  cmp #232
-  bne .return_0
-  ; yes
-  lda #1
-  jmp after_screen_end_check
-.return_0
-  lda #0
-  jmp after_screen_end_check
-
-
 ;;;;;;;;;;;;;;;;;;; Data
 
 ; Smoother color gradient
-colors !byte 11, 11, 12, 15, 1, 7, 13, 3, 4, 14, 6, 2, 10, 9, 8, 5
+colors
+  !byte 11, 12, 15, 1, 7, 13, 3, 4, 14, 6, 2, 10, 9, 8, 5
+  !byte 11, 12, 15, 1, 7, 13, 3, 4, 14, 6, 2, 10, 9, 8, 5
 
 init_color_indices
   !byte  0, 0, 0, 1, 1, 1, 2, 2, 2, 3
@@ -250,73 +195,79 @@ rasterlines
   !byte 50 + (42 * 0) - PREP_LINES
 
 sprite_y_values
-  !for i, 0, 4 {
-    !byte 50 + (42 * i)
-  }
+  !byte 92, 134, 176, 218, 218
+  !byte 50,  92, 134, 176, 218
+  !byte 50,  92, 134, 176, 218
+  !byte 50,  92, 134, 176, 218
+  !byte 50,  92, 134, 176, 218
+  !byte 50,  92, 134, 176, 218
+  !byte 50,  92, 134, 176, 218
+  !byte 92, 134, 176, 218, 218
 
 sprite_x1_low_values
-  !byte 20, 19, 18, 17, 16
-  !byte 60, 60 - 24, 60 - 24*2, 60 - 24*2, 60 - 24*2
-  !byte 100, 100 - 24, 100 - 24*2, 100 - 24*3, 100 - 24*4
-  !byte 140, 140 - 24, 140 - 24*2, 140 - 24*3, 140 - 24*4
-  !byte 180, 180 + 24, 180 + 24*2, 180 + 24*3, (180 + 24*4)%$ff
-  !byte 220, 220 + 24, (220 + 24*2)%$ff, (220 + 24*3)%$ff, (220 + 24*4)%$ff
-  !byte 5, 5 + 24, 5 + 24*2, 5 + 24*2, 5 + 24*2
-  !byte 45, 45, 45, 45, 45
+  !byte 116 %$ff, 100 %$ff,  84 %$ff,  68 %$ff,  52 %$ff
+  !byte  36 %$ff,  20 %$ff,   4 %$ff,   0 %$ff,   0 %$ff
+  !byte  84 %$ff,  68 %$ff,  52 %$ff,  36 %$ff,  20 %$ff
+  !byte 132 %$ff, 116 %$ff, 100 %$ff,  84 %$ff,  68 %$ff
+  !byte 180 %$ff, 196 %$ff, 212 %$ff, 228 %$ff, 244 %$ff
+  !byte 228 %$ff, 244 %$ff, 260 %$ff, 276 %$ff, 292 %$ff
+  !byte 276 %$ff, 292 %$ff, 308 %$ff, 324 %$ff, 340 %$ff
+  !byte 196 %$ff, 212 %$ff, 228 %$ff, 244 %$ff, 260 %$ff
+
 sprite_x_high_values
-  !byte %11000000
-  !byte %11000000
+  !byte %01000000
+  !byte %01000000
+  !byte %01100000
+  !byte %01100000
   !byte %11100000
-  !byte %11100000
-  !byte %11110000
 
 
 ; sprite 1
 *=$2000
+  !byte %11111111,%11111111,%11111111
+  !byte %11111111,%11111111,%11111110
   !byte %11111111,%11111111,%11111110
   !byte %11111111,%11111111,%11111100
   !byte %11111111,%11111111,%11111100
   !byte %11111111,%11111111,%11111000
   !byte %11111111,%11111111,%11111000
+  !byte %11111111,%11111111,%11111000
+  !byte %11111111,%11111111,%11110000
   !byte %11111111,%11111111,%11110000
   !byte %11111111,%11111111,%11110000
   !byte %11111111,%11111111,%11100000
   !byte %11111111,%11111111,%11100000
+  !byte %11111111,%11111111,%11100000
+  !byte %11111111,%11111111,%11000000
+  !byte %11111111,%11111111,%11000000
   !byte %11111111,%11111111,%11000000
   !byte %11111111,%11111111,%10000000
   !byte %11111111,%11111111,%10000000
   !byte %11111111,%11111111,%00000000
   !byte %11111111,%11111111,%00000000
-  !byte %11111111,%11111110,%00000000
-  !byte %11111111,%11111110,%00000000
-  !byte %11111111,%11111100,%00000000
-  !byte %11111111,%11111100,%00000000
-  !byte %11111111,%11111000,%00000000
-  !byte %11111111,%11111000,%00000000
-  !byte %11111111,%11110000,%00000000
   !byte 0
 ; sprite 2
+  !byte %11111111,%11111111,%11111111
+  !byte %01111111,%11111111,%11111111
   !byte %01111111,%11111111,%11111111
   !byte %00111111,%11111111,%11111111
   !byte %00111111,%11111111,%11111111
   !byte %00011111,%11111111,%11111111
   !byte %00011111,%11111111,%11111111
+  !byte %00011111,%11111111,%11111111
+  !byte %00001111,%11111111,%11111111
   !byte %00001111,%11111111,%11111111
   !byte %00001111,%11111111,%11111111
   !byte %00000111,%11111111,%11111111
   !byte %00000111,%11111111,%11111111
+  !byte %00000111,%11111111,%11111111
+  !byte %00000011,%11111111,%11111111
+  !byte %00000011,%11111111,%11111111
   !byte %00000011,%11111111,%11111111
   !byte %00000001,%11111111,%11111111
   !byte %00000001,%11111111,%11111111
   !byte %00000000,%11111111,%11111111
   !byte %00000000,%11111111,%11111111
-  !byte %00000000,%01111111,%11111111
-  !byte %00000000,%01111111,%11111111
-  !byte %00000000,%00111111,%11111111
-  !byte %00000000,%00111111,%11111111
-  !byte %00000000,%00011111,%11111111
-  !byte %00000000,%00011111,%11111111
-  !byte %00000000,%00001111,%11111111
   !byte 0
   !fill 64, $ff
   !fill 64, $ff
@@ -395,58 +346,26 @@ SR_init_irqs
 
 !zone
 SR_fill_screen
-  lda #<CHAR_START
-  sta PTR1
-  lda #>CHAR_START
-  sta PTR1_HIGH
-
-  lda #<COLOR_START
-  sta PTR2
-  lda #>COLOR_START
-  sta PTR2_HIGH
-
-  lda #0
-  sta current_x
--
-  lda PTR1
-  ; Write only lower chars
-  and #%01111111
-
-  ; Draw character
-  ldy ptr_idx
-  sta (PTR1), y
-
-  ; Color
-  ldy current_x
-  lda init_color_indices, y
-  tay
-  lda colors, y
-  ldy #0
-  sta (PTR2), y
-
-  inc current_x
-  lda current_x
-  cmp #40
-  bne skip_reset_current_x
-  lda #0
-  sta current_x
-
-skip_reset_current_x
-  jmp bump_pointers
-after_bump_pointers
-  cmp #0
-  beq -
-  ; end of screen reached
-
-  ; erase sides
-  lda #32
+  lda #102
+  ldy #10
+.loop1
   !for i, 0, 24 {
-    !for j, 0, 4 {
-      sta CHAR_START + (i * 40) + j
-    }
-    !for j, 35, 39 {
-      sta CHAR_START + (i * 40) + j
-    }
+    sta CHAR_START + (i * 40), y
   }
+  iny
+  cpy #29
+  bne .loop1
 
+  ldy #10
+  ldx #0
+.loop2
+  !for i, 0, 24 {
+    lda colors, y
+    sta COLOR_START + (i * 40), y
+  }
+  iny
+  cpy #29
+  beq .done
+  jmp .loop2
+.done
   rts
