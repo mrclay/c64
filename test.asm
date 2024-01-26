1a77ce078d944ca11c7a7b99a9987350377399a9
Steve Clay <steve@mrclay.org>
2024-01-25 22:31:21 -0500

    Pattern

--- test.asm
+++ test.asm
@@ -28,7 +28,7 @@
 
   ; How many lines before the target raster line before we start
   ; moving sprites
-  PREP_LINES = 15
+  PREP_LINES = 16
 
 main
   jsr SR_screen_setup
@@ -53,31 +53,29 @@ SR_screen_setup
 
   ; sprite FG colors
   !for i, 0, 7 {
-    lda #i + 1
+    lda #0
     sta $D027 + i
   }
 
   ; point to our sprite data
-  lda #SPRITE_DATA_SQUARE
+  lda #SPRITE_DATA_L
   sta SPRITE_PTR1
   sta SPRITE_PTR1 + 1
   sta SPRITE_PTR1 + 2
+  sta SPRITE_PTR1 + 3
+  sta SPRITE_PTR1 + 4
   sta SPRITE_PTR1 + 5
   sta SPRITE_PTR1 + 6
   sta SPRITE_PTR1 + 7
-  lda #SPRITE_DATA_L
-  sta SPRITE_PTR1 + 3
-  lda #SPRITE_DATA_R
-  sta SPRITE_PTR1 + 4
 
   ; turn on sprites
-  lda #%10111101
+  lda #%11111111
   sta $d015
 
   ; sprite X values
   ldy #0
-  !for i, 0, 8 {
-    lda sprite_x1_low_values + (5 * i), y
+  !for i, 0, 7 {
+    lda sprite_x1_low_values + i, y
     sta SPRITE_X1 + (i * 2)
   }
   lda sprite_x_high_values
@@ -103,33 +101,15 @@ irq_handler
   jmp .irq_not_5
 
 .irq_5
-  ; bump frame count
-  lda frame
-  clc
-  adc #1
-  bne .skip_frame_rollover
-  inc frame + 1
-  lda #0
-
-.skip_frame_rollover
-  sta frame
-  jsr SR_after_screen
+  inc frame
+  ; jsr SR_move_colors
   jmp .done
 
 .irq_not_5
-  ; sprite X values
-  ldy active_interrupt
-  !for i, 0, 7 {
-    lda sprite_x1_low_values + (5 * i), y
-    sta SPRITE_X1 + (i * 2)
-  }
-  lda sprite_x_high_values, y
-  sta SPRITE_X_HIGH
-
   ; sprite Y values
   ldy active_interrupt
   !for i, 0, 7 {
-    lda sprite_y_values + (5 * i), y
+    lda sprite_y_values, y
     sta SPRITE_X1 + 1 + (i * 2)
   }
 
@@ -162,12 +142,8 @@ SR_after_screen
 colors
   !byte 11, 12, 15, 1, 7, 13, 3, 4, 14, 6, 2, 10, 9, 8, 5
   !byte 11, 12, 15, 1, 7, 13, 3, 4, 14, 6, 2, 10, 9, 8, 5
+  !byte 11, 12, 15, 1, 7, 13, 3, 4, 14, 6, 2, 10, 9, 8, 5
 
-init_color_indices
-  !byte  0, 0, 0, 1, 1, 1, 2, 2, 2, 3
-  !byte  3, 3, 4, 4, 4, 5, 5, 5, 6, 6
-  !byte  6, 7, 7, 7, 8, 8, 8, 9, 9, 9
-  !byte 10,10,11,11,12,12,13,13,14,14
 color_idx !byte 0
 about !scr "mrclay.org nov 2023"
   !byte 0
@@ -183,7 +159,7 @@ active_letter_block !byte 0
 idx_in_active_set !byte 0
 screen_writes !byte 0
 temp_line !fill 40
-frame !fill 2, 0
+frame !byte 0
 
 active_interrupt !byte 0
 
@@ -192,83 +168,52 @@ rasterlines
     !byte 50 + (42 * i) - PREP_LINES
   }
   !byte 210
-  !byte 50 + (42 * 0) - PREP_LINES
+  !byte 50 - PREP_LINES
 
 sprite_y_values
-  !byte 92, 134, 176, 218, 218
-  !byte 50,  92, 134, 176, 218
-  !byte 50,  92, 134, 176, 218
-  !byte 50,  92, 134, 176, 218
-  !byte 50,  92, 134, 176, 218
-  !byte 50,  92, 134, 176, 218
-  !byte 50,  92, 134, 176, 218
-  !byte 92, 134, 176, 218, 218
+  !for i, 0, 4 {
+    !byte 50 + (42 * i)
+  }
 
 sprite_x1_low_values
-  !byte 116 %$ff, 100 %$ff,  84 %$ff,  68 %$ff,  52 %$ff
-  !byte  36 %$ff,  20 %$ff,   4 %$ff,   0 %$ff,   0 %$ff
-  !byte  84 %$ff,  68 %$ff,  52 %$ff,  36 %$ff,  20 %$ff
-  !byte 132 %$ff, 116 %$ff, 100 %$ff,  84 %$ff,  68 %$ff
-  !byte 180 %$ff, 196 %$ff, 212 %$ff, 228 %$ff, 244 %$ff
-  !byte 228 %$ff, 244 %$ff, 260 %$ff, 276 %$ff, 292 %$ff
-  !byte 276 %$ff, 292 %$ff, 308 %$ff, 324 %$ff, 340 %$ff
-  !byte 196 %$ff, 212 %$ff, 228 %$ff, 244 %$ff, 260 %$ff
+  !byte  24 %$ff
+  !byte  64 %$ff
+  !byte 104 %$ff
+  !byte 144 %$ff
+  !byte 184 %$ff
+  !byte 224 %$ff
+  !byte 264 %$ff
+  !byte 304 %$ff
 
 sprite_x_high_values
-  !byte %01000000
-  !byte %01000000
-  !byte %01100000
-  !byte %01100000
-  !byte %11100000
+  !byte %11000000
 
 
 ; sprite 1
 *=$2000
-  !byte %11111111,%11111111,%11111111
-  !byte %11111111,%11111111,%11111110
-  !byte %11111111,%11111111,%11111110
-  !byte %11111111,%11111111,%11111100
-  !byte %11111111,%11111111,%11111100
-  !byte %11111111,%11111111,%11111000
-  !byte %11111111,%11111111,%11111000
-  !byte %11111111,%11111111,%11111000
-  !byte %11111111,%11111111,%11110000
-  !byte %11111111,%11111111,%11110000
-  !byte %11111111,%11111111,%11110000
-  !byte %11111111,%11111111,%11100000
-  !byte %11111111,%11111111,%11100000
-  !byte %11111111,%11111111,%11100000
-  !byte %11111111,%11111111,%11000000
-  !byte %11111111,%11111111,%11000000
-  !byte %11111111,%11111111,%11000000
-  !byte %11111111,%11111111,%10000000
-  !byte %11111111,%11111111,%10000000
-  !byte %11111111,%11111111,%00000000
-  !byte %11111111,%11111111,%00000000
+!byte %11110000,%10010000,%00000000
+!byte %11110000,%10010000,%01100000
+!byte %11110000,%10010000,%01100000
+!byte %11110000,%11110000,%00000000
+!byte %00001111,%00001111,%00000000
+!byte %00001111,%00001001,%00000000
+!byte %00001111,%00001001,%00000000
+!byte %00001111,%00001111,%00000000
+!byte %11110000,%11110000,%11110000
+!byte %00010000,%11110000,%10000000
+!byte %00010000,%11110000,%10000000
+!byte %11110000,%11110000,%11110000
+!byte %00001111,%00001111,%00000000
+!byte %00001001,%00001111,%00000000
+!byte %00001001,%00001111,%00000000
+!byte %00001111,%00001111,%00000000
+!byte %00000000,%11110000,%11110000
+!byte %01100000,%10010000,%11110000
+!byte %01100000,%10010000,%11110000
+!byte %00000000,%10010000,%11110000
+!byte %00000000,%00000000,%00000000
   !byte 0
-; sprite 2
-  !byte %11111111,%11111111,%11111111
-  !byte %01111111,%11111111,%11111111
-  !byte %01111111,%11111111,%11111111
-  !byte %00111111,%11111111,%11111111
-  !byte %00111111,%11111111,%11111111
-  !byte %00011111,%11111111,%11111111
-  !byte %00011111,%11111111,%11111111
-  !byte %00011111,%11111111,%11111111
-  !byte %00001111,%11111111,%11111111
-  !byte %00001111,%11111111,%11111111
-  !byte %00001111,%11111111,%11111111
-  !byte %00000111,%11111111,%11111111
-  !byte %00000111,%11111111,%11111111
-  !byte %00000111,%11111111,%11111111
-  !byte %00000011,%11111111,%11111111
-  !byte %00000011,%11111111,%11111111
-  !byte %00000011,%11111111,%11111111
-  !byte %00000001,%11111111,%11111111
-  !byte %00000001,%11111111,%11111111
-  !byte %00000000,%11111111,%11111111
-  !byte %00000000,%11111111,%11111111
-  !byte 0
+  !fill 64, $ff
   !fill 64, $ff
   !fill 64, $ff
   !fill 64, $ff
@@ -296,21 +241,26 @@ SR_wrap_lines
 
 
 !zone
-SR_wrap_colors
-  !for j, 0, 24 {
-    lda COLOR_START + (j * 40)
-    sta temp_line + j
+SR_move_colors
+  ldy #0
+  lda frame
+  ror
+  ror
+  tax
+.loop
+  txa
+  and #%00001111
+  tax
+  !for i, 0, 24 {
+    lda colors, x
+    sta COLOR_START + (i * 40), y
   }
-  !for i, 0, 38 {
-    !for j, 0, 24 {
-      lda COLOR_START + (j * 40) + i + 1
-      sta COLOR_START + (j * 40) + i
-    }
-  }
-  !for j, 0, 24 {
-    lda temp_line + j
-    sta COLOR_START + (j * 40) + 39
-  }
+  inx
+  iny
+  cpy #39
+  beq .done
+  jmp .loop
+.done
   rts
 
 
@@ -346,25 +296,24 @@ SR_init_irqs
 
 !zone
 SR_fill_screen
-  lda #102
-  ldy #10
+  lda #32 | %10000000
+  ldy #0
 .loop1
   !for i, 0, 24 {
     sta CHAR_START + (i * 40), y
   }
   iny
-  cpy #29
+  cpy #39
   bne .loop1
 
-  ldy #10
-  ldx #0
+  ldy #0
 .loop2
   !for i, 0, 24 {
     lda colors, y
     sta COLOR_START + (i * 40), y
   }
   iny
-  cpy #29
+  cpy #39
   beq .done
   jmp .loop2
 .done
